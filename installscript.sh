b783b8362680d0c5adc1876176b7caf3fafc360b
Madan Kumar <madankumar.vijayakumar@sjsu.edu>
2015-11-20 14:04:57 -0800

    script

---
+++ installscript.sh
@@ -0,0 +1,3 @@
+sudo apt-get install rpm
+sudo apt-get install alien
+wget "http://bdestorage.blob.core.windows.net/rpm/InformaticaHadoop-9.6.1.HF3-1.2323.x86_64.tar.gz?sv=2014-02-14&sr=c&sig=7D2ZsZ%2BX%2FI4cNIIbxbXmohhOMe0iYoYEjX%2Ba2yyVUTY%3D&st=2015-11-20T08%3A00%3A00Z&se=2015-11-28T08%3A00%3A00Z&sp=r" -O InformaticaHadoop.tar
